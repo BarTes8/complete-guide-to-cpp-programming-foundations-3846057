bac4b3a9e69a19a190b80c463d378a07adf4923b
Barbara Tessema <74462021+BarTes8@users.noreply.github.com>
2025-09-09 17:02:18 +0000

    more changes

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -5,15 +5,21 @@
 #include <iostream>
 #include <cstdint>
 
+#define MAX_AMMO 500
+#define DEBUG
+
 int main(){
-    int32_t ammo = 100;
+    int32_t ammo = MAX_AMMO / 5;
     uint8_t health_items = 5;
 
+#ifdef DEBUG
+    std::cout << "[DEBUG] Starging game simulation..." << std::endl;
+#endif
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
 
     std::cout << "Final Ammo: " << ammo << std::endl;
-    std::cout << "Remaining Health Items: " << (int) health_items << std::endl;
+    std::cout << "Remaining Health Items: " <<  (int) health_items << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
